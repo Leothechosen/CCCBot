799787cb9872fe8a7f17919d365c7d9b499e9d36
Leothechosen <Leosmarty98@gmail.com>
2020-05-01 07:41:23 -0700

    Added user search, logging

--- apirequests.py
+++ apirequests.py
@@ -1,11 +1,25 @@
 import aiohttp
+import logging
+
+logger = logging.getLogger(f"CCCBot.{__name__}")
 
 async def ccc_search(ctx, order):
     async with aiohttp.ClientSession() as session:
         async with session.get(f"https://www.castingcall.club/api/v1/search?order_by={order}") as response:
             if response.status != 200:
                 await ctx.send(f"CCC API returned a {response.status} error")
-                logging.warning(f"CCC API returned a {response.status} error. Order: {order}")
+                logging.warning(f"CCC API returned a {response.status} error. Context: {ctx.message.content}")
+                return
+            response = await response.json()
+            await session.close()
+    return response
+
+async def ccc_users(ctx, name, page_num):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"https://www.castingcall.club/api/v1/voicesearch?contains={name}") as response:
+            if response.status != 200:
+                await ctx.send(f"CCC API returned a {response.status} error")
+                logging.warning(f"CCC API returend a {response.status} error. Context: {ctx.message.content}")
                 return
             response = await response.json()
             await session.close()
